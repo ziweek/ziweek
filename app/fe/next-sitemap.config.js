dc18c6847e26cd1e5df4b68e2925e7fdb818ded4
ziweek <alex.jiuk.kim@gmail.com>
2024-09-17 18:49:07 +0900

    📈 Add GA & GCS

--- app/fe/next-sitemap.config.js
+++ app/fe/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl: "https://ziweek.vercel.app/",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
