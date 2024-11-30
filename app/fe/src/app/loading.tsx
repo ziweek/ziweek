be8ac71cc4186844a3e8d260618f4d772f8f162c
ziweek <alex.jiuk.kim@gmail.com>
2024-12-01 00:57:46 +0900

    🎉

--- app/fe/src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress } from "@nextui-org/react";
-
-export default function loading(props: any) {
-  return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <CircularProgress></CircularProgress>
-    </div>
-  );
-}
