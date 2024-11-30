7e27e13f3b3193804cae9595a06c9da4c9a46916
ziweek <alex.jiuk.kim@gmail.com>
2024-12-01 01:09:25 +0900

    .

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,51 +1,29 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  extends: [
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
-    'plugin:prettier/recommended',
-    'prettier/react',
-    'prettier/@typescript-eslint',
-  ],
-  plugins: ['@typescript-eslint', 'prettier', 'react-hooks'],
-  parserOptions: {
-    ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es6": true
     },
-    project: './tsconfig.json',
-  },
-  env: {
-    browser: true,
-    jest: true,
-    node: true,
-  },
-  globals: {
-    // cy: true,
-    // Cypress: true,
-  },
-  rules: {
-    'no-underscore-dangle': 0,
-    '@typescript-eslint/no-namespace': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/no-use-before-define': 0,
-    'import/prefer-default-export': 0,
-    'import/extensions': 0,
-    'react/react-in-jsx-scope': 0,
-    quotes: [
-      2,
-      'single',
-      {
-        avoidEscape: true,
-      },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
     ],
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx', '.tsx'],
-      },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
     ],
-  },
+    "rules": {
+    }
 };
